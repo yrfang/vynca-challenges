a62e7e954d8c56976d821c5b4e475577a635f23d
YR Fang <fyr1116@gmail.com>
2018-04-27 07:26:07 +0800

    modifying store with redux

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import createLogger from 'redux-logger';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStore(
+  reducers,
+  applyMiddleware(reduxThunk, createLogger)
+);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
   , document.getElementById('app'));
